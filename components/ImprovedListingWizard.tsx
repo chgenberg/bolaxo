d1ac1604b9d7777ab368ed2ee199329e24823ace
Christopher Genberg <christopher@1753skincare.com>
2025-11-10 12:41:45 +0100

    Fix JSX structure: add missing closing div tag for Content section in preview

--- components/ImprovedListingWizard.tsx
+++ components/ImprovedListingWizard.tsx
@@ -1288,6 +1288,7 @@ export default function ImprovedListingWizard({ onClose }: WizardProps) {
               </div>
             </div>
           </div>
+          </div>
           
           {/* Footer */}
           <div className="px-8 py-6 border-t border-gray-200 bg-white rounded-b-2xl">
