773ecbb7de59dd31d658f49fe8d476ccb9fd01d6
Christopher Genberg <christopher@1753skincare.com>
2025-11-14 03:53:31 +0100

    Force dynamic analyze landing pages

--- app/analysera/page.tsx
+++ app/analysera/page.tsx
@@ -1,5 +1,8 @@
 import AnalyzeLandingPage from '@/components/AnalyzeLandingPage'
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export default function AnalyzePage() {
   return <AnalyzeLandingPage />
 }
